cf8d1004c82284adde8116d9438bf53bb758943f
ValSyacy <novalsyacy11@gmail.com>
2024-02-27 19:58:21 +0700

    percobaan 3

--- buku02.java
+++ buku02.java
@@ -1,14 +1,25 @@
-public class buku02{
+public class buku02 {
     
     String judul, pengarang;
     int halaman, stok, harga;
 
+    public buku02() {
+    }
+
+    public buku02(String judul, String pengarang, int halaman, int stok, int harga) {
+        this.judul = judul;
+        this.pengarang = pengarang;
+        this.halaman = halaman;
+        this.stok = stok;
+        this.harga = harga;
+    }
+
     void tampilInformasi() {
-        System.out.println("Judul :" + judul);
-        System.out.println("Pengarang :" + pengarang);
-        System.out.println("Jumlah halaman :" + halaman);
-        System.out.println("Sisa stok :" + stok);
-        System.out.println("Harga :Rp. " + harga);
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah halaman: " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: Rp. " + harga);
     }
 
     void terjual(int jml) {
@@ -19,12 +30,12 @@ public class buku02{
             System.out.println("Stok habis, tidak dapat menjual buku.");
         }
     }
-    
+
     void restock(int jml) {
         stok += jml;
     }
 
-    void gantiHarga(int hrg){
+    void gantiHarga(int hrg) {
         harga = hrg;
     }
 }
--- bukuMain02.java
+++ bukuMain02.java
@@ -12,5 +12,12 @@ public class bukuMain02 {
         bk1.terjual(5);
         bk1.gantiHarga(60000);
         bk1.tampilInformasi();
+
+        buku02 bk2 = new buku02("self reward", "Maheera Ayesha", 160, 29, 59000);
+        bk2.terjual(11);
+        bk2.tampilInformasi();
+
+        buku02 bukualfitrohsn = new buku02("Cara Menjadi Kaya Tanpa Bekerja", "Alfitroh Syacy Novalino", 200, 10, 50000);
+        bukualfitrohsn.tampilInformasi();
     }   
 }
