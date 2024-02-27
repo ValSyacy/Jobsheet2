c5648bd3b173eab6abc42df3f5f1e42f375f9e24
ValSyacy <novalsyacy11@gmail.com>
2024-02-27 18:54:50 +0700

    Percobaan 1 dan 2

---
+++ buku02.java
@@ -0,0 +1,30 @@
+public class buku02{
+    
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi() {
+        System.out.println("Judul :" + judul);
+        System.out.println("Pengarang :" + pengarang);
+        System.out.println("Jumlah halaman :" + halaman);
+        System.out.println("Sisa stok :" + stok);
+        System.out.println("Harga :Rp. " + harga);
+    }
+
+    void terjual(int jml) {
+        if (stok > 0) {
+            stok -= jml;
+            System.out.println(jml + " buku terjual.");
+        } else {
+            System.out.println("Stok habis, tidak dapat menjual buku.");
+        }
+    }
+    
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg){
+        harga = hrg;
+    }
+}
